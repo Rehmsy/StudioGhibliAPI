50e277beab6fab286b8a0f0e3d3a0b51c766222e
Rehmsy <sarahrehmer@gmail.com>
2019-03-04 15:39:46 -0800

    fixed href in films-component.test.js

--- test/films-component.test.js
+++ test/films-component.test.js
@@ -18,7 +18,7 @@ test('make film template', assert => {
   assert.htmlEqual(dom, /*html*/`
     <li class="film" title="In the latter part of World War II, a boy and his sister, orphaned when their mother is killed in the firebombing of Tokyo, are left to survive on their own in what remains of civilian life in Japan. The plot follows this boy and his sister as they do their best to survive in the Japanese countryside, battling hunger, prejudice, and pride in their own quiet, personal battle.">
       <h1>
-        <a href="./film-detail.html?id=12cfb892-aac0-4c5b-94af-521852e46d6a">Grave of the Fireflies</a>
+        <a href="./film-detail-view.html?id=12cfb892-aac0-4c5b-94af-521852e46d6a">Grave of the Fireflies</a>
       </h1>
     </li>  
       `);
